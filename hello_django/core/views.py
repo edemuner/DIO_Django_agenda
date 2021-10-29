8848a3f9aaa3ca116b2fa10e8e9534d2f49af106
edemuner <eduardood15@hotmail.com>
2021-10-28 21:11:03 -0300

    pequeno exercício, criando funções para retornar as operações aritméticas básicas, fornecendo os 2 números para a operação através da URL, criei uma URL para cada função de views

--- hello_django/core/views.py
+++ hello_django/core/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse(f'<h1>Hello {nome}!</h1> a idade informada é {idade}')
+
+def soma(request, num1, num2):
+    return HttpResponse(num1 + num2)
+
+def subtracao(request, num1, num2):
+    return HttpResponse(num1 - num2)
+
+def multiplicacao(request, num1, num2):
+    return HttpResponse(num1 * num2)
+
+def divisao(request, num1, num2):
+    if num2 != 0:
+        return HttpResponse(num1 / num2)
+    else:
+        return HttpResponse('Impossível dividir por zero!')
