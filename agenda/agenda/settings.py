5af0b0d266c4ffcc93f7af7781b1af1f5424718a
edemuner <eduardood15@hotmail.com>
2021-11-08 18:50:56 -0300

    Criado o primeiro template, agenda.html

--- agenda/agenda/settings.py
+++ agenda/agenda/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'agenda.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # foi registrado o dir acima, porém não aconteceu o erro que aconteceu na aula de referência
+        # o template estava carregando sem esse registro
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
