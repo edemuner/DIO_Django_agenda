cdeef10bea460006774463d539d2947733b7ebed
edemuner <eduardood15@hotmail.com>
2021-11-13 16:01:32 -0300

    configurada view de delete evento para retornar um 404 caso seja fornecida uma URL relativa a um evento não existente, ou se for tentado acessar por um usuário sem permissão

--- agenda/core/views.py
+++ agenda/core/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 
 # Create your views here.
 
@@ -82,7 +83,12 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
     if usuario == evento.usuario:
         evento.delete()
+    else:
+        raise Http404()
     return redirect('/')
